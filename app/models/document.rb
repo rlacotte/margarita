414bd9832d3c69a33d5fa47279d398710e87e2a2
Renaud de Lacotte <renauddelacotte@MacBook-Pro-de-Renaud.local>
2018-01-06 17:34:55 +0100

    Companies Show

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,5 +2,8 @@ class Document
   include Mongoid::Document
   field :document_name, type: String
   field :document_type, type: String
+
   mount_uploader :file, DocumentUploader
+
+  belongs_to :company
 end
